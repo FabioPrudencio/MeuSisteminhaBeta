9cdc7536f63d35a0ebb717d7fe3f858f8c9fbc47
FabioPrudencio <fabio.oliveira.prudencio@gmail.com>
2022-11-06 01:17:22 -0300

    Testando a primeira classe de domínio

---
+++ src/main/java/com/ornitorrincosystems/meusisteminha/entities/People.java
@@ -0,0 +1,112 @@
+package com.ornitorrincosystems.meusisteminha.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+public class People implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+	private String completeName;
+	private String cpf;
+	private String cnpj;
+	private Date birthDate;
+	private String phoneNumber;
+	private String email;
+
+	public People() {
+	}
+
+	public People(Integer id, String completeName, String cpf, String cnpj, Date birthDate, String phoneNumber,
+			String email) {
+		super();
+		this.id = id;
+		this.completeName = completeName;
+		this.cpf = cpf;
+		this.cnpj = cnpj;
+		this.birthDate = birthDate;
+		this.phoneNumber = phoneNumber;
+		this.email = email;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getCompleteName() {
+		return completeName;
+	}
+
+	public void setCompleteName(String completeName) {
+		this.completeName = completeName;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		People other = (People) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "People [id=" + id + ", completeName=" + completeName + ", cpf=" + cpf + ", cnpj=" + cnpj
+				+ ", birthDate=" + birthDate + ", phoneNumber=" + phoneNumber + ", email=" + email + "]";
+	}
+
+}
--- src/main/java/com/ornitorrincosystems/meusisteminha/resources/PeopleResource.java
+++ src/main/java/com/ornitorrincosystems/meusisteminha/resources/PeopleResource.java
@@ -1,16 +1,29 @@
 package com.ornitorrincosystems.meusisteminha.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ornitorrincosystems.meusisteminha.entities.People;
 
 @RestController
 @RequestMapping(value="/peoples")
 public class PeopleResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listPeoples() {
-		return "Rest está funcionando";
+	public List<People> listAll() {
+		
+		People p1 = new People(1, "Fabio de Oliveira Prudencio", "07138744900", null, null, "47992090158", "fabio.oliveira.prudencio@gmail.com");
+		People p2 = new People(2, "Giulia Grasielle Lütke", null, null, null, null, "giulialutke@gmail.com");
+		
+		List<People> list = new ArrayList<>();
+		list.add(p1);
+		list.add(p2);		
+		
+		return list;
 	}
 	
 }
