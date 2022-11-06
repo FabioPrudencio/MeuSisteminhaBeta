e3acd5824cf19f24f695cffa884302a38fc8af37
FabioPrudencio <fabio.oliveira.prudencio@gmail.com>
2022-11-05 22:23:06 -0300

    Projeto Criado

---
+++ src/main/java/com/ornitorrincosystems/meusisteminha/MeusisteminhaApplication.java
@@ -0,0 +1,13 @@
+package com.ornitorrincosystems.meusisteminha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeusisteminhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeusisteminhaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ornitorrincosystems/meusisteminha/MeusisteminhaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ornitorrincosystems.meusisteminha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeusisteminhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
