1da74bc5cb2ef44f9a98391db180ea30eb952343
FabioPrudencio <fabio.oliveira.prudencio@gmail.com>
2022-11-06 00:33:15 -0300

    Rest Testado

---
+++ src/main/java/com/ornitorrincosystems/meusisteminha/resources/PeopleResource.java
@@ -0,0 +1,16 @@
+package com.ornitorrincosystems.meusisteminha.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/peoples")
+public class PeopleResource {
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public String listPeoples() {
+		return "Rest está funcionando";
+	}
+	
+}
