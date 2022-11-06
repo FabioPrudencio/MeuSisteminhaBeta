56d259262e0413811b453b0301fd526ae20b671f
FabioPrudencio <fabio.oliveira.prudencio@gmail.com>
2022-11-06 11:43:26 -0300

    Criando operação de Instanciação

--- src/main/java/com/ornitorrincosystems/meusisteminha/MeusisteminhaApplication.java
+++ src/main/java/com/ornitorrincosystems/meusisteminha/MeusisteminhaApplication.java
@@ -1,13 +1,33 @@
 package com.ornitorrincosystems.meusisteminha;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.ornitorrincosystems.meusisteminha.entities.People;
+import com.ornitorrincosystems.meusisteminha.repositories.PeopleRepository;
+
 @SpringBootApplication
-public class MeusisteminhaApplication {
+public class MeusisteminhaApplication implements CommandLineRunner {
+	
+	@Autowired
+	PeopleRepository peopleRepo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MeusisteminhaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		People p1 = new People(null, "Fabio de Oliveira Prudencio", "07138744900", null, null, "47992090158", "fabio.oliveira.prudencio@gmail.com");
+		People p2 = new People(null, "Giulia Grasielle Lütke", null, null, null, null, "giulialutke@gmail.com");
+		
+		peopleRepo.saveAll(Arrays.asList(p1,p2));
+		
 	}
 
 }
